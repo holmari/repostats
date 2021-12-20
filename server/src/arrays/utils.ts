1caacced0777c77aec1b4794ff73fbe860e7d981
Lasse Holmstedt <lasse.holmstedt@gmail.com>
2021-12-20 09:50:37 +0100

    Fix typo in comment

--- server/src/arrays/utils.ts
+++ server/src/arrays/utils.ts
@@ -1,6 +1,6 @@
 import {uniq} from 'lodash';
 
-// Note: this assumees reference equality for objects
+// Note: this assumes reference equality for objects
 export function removeDuplicates<T>(data: ReadonlyArray<T>): ReadonlyArray<T> {
   return uniq(data);
 }
