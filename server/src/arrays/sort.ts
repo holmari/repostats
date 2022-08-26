25838e165fc3dc78cf2d5f44d9e95deed2c3cab1
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-26 17:37:25 +0800

    Delete server directory

--- server/src/arrays/sort.ts
+++
@@ -1,3 +0,0 @@
-export function descByCreationDate<T extends {createdAt: string}>(left: T, right: T): number {
-  return -left.createdAt.localeCompare(right.createdAt);
-}
--- server/src/types/download.ts
+++
@@ -1,8 +0,0 @@
-import {AxiosInstance} from 'axios';
-import {DownloadRequest, GithubConnector, RepoConfig} from './types';
-
-export interface DownloadContext {
-  readonly repoConfig: RepoConfig<GithubConnector>;
-  readonly http: AxiosInstance;
-  readonly request: DownloadRequest;
-}
